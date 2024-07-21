03b459e9b20a3b30f6c9a509cd39cfc23a23f908
ichigo101101 <1429553410@qq.com>
2024-07-21 13:47:00 +0900

    p8 增删改好了，图片加载不出来

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -3,10 +3,12 @@ package com.example.controller;
 import com.example.common.Result;
 import com.example.entity.Book;
 import com.example.entity.Params;
+import com.example.exception.CustomException;
 import com.example.service.BookService;
 import com.github.pagehelper.PageInfo;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
+
 
 
 
@@ -26,12 +28,23 @@ public class BookController {
 
     @PostMapping
     public Result save(@RequestBody Book book) {
-        if (book.getId() == null) {
-            bookService.add(book);
-        } else {
-            bookService.update(book);
+//        if (book.getId() == null) {
+//            bookService.add(book);
+//        } else {
+//            bookService.update(book);
+//        }
+//        return Result.success();
+
+        try {
+            if (book.getId() == null) {
+                bookService.add(book);
+            } else {
+                bookService.update(book);
+            }
+            return Result.success();
+        } catch (CustomException e) {
+            return Result.error(e.getMsg());
         }
-        return Result.success();
     }
 
     @DeleteMapping("/{id}")
